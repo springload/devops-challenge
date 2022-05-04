833244afc948830b8ea96e654d67d375f13c5331
MT <mt.at.github@gmail.com>
2022-05-04 17:16:53 +1200

    Updated main.go...   To be more verbose;   To avoid the re-use of var;   With extra comments

--- 3-golang/main.go
+++ 3-golang/main.go
@@ -10,11 +10,14 @@ func main() {
 	var wg sync.WaitGroup
 	wg.Add(5)
 	for i := 0; i < 5; i++ {
-		go func() {
-			fmt.Println(i)
+		// Avoid re-use of var - ref: https://go.dev/doc/faq#closures_and_goroutines
+		go func(t int) {
+			fmt.Println("Start thread: ", t)
+			// Pretend workload here
 			time.Sleep(1 * time.Second)
+			fmt.Println("End thread: ", t)
 			wg.Done()
-		}()
+		}(i)
 	}
 	wg.Wait()
 }
