5cc00b53d2f36d261bcd45bd7792a6da48450d4d
Abdur Rafay <rafay.atif@modicagroup.com>
2022-10-08 15:18:51 +1300

    Commit for Bash Challenge

--- 4-bash/conditions.sh
+++ 4-bash/conditions.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
-
 a="test"
 # this will print ok
 if [ "$a" = "test" ]; then echo ok; fi
 # this will print ok too
 if [ "$a" == "test" ]; then echo ok; fi
 # what's the difference?
+
+#Both run the same way on my machine(macos).Referring to the man pages for "test" it has the below entry
+#For compatibility with some other implementations, the "=" primary can be substituted with "==" with the same meaning.
+#however running 'sh conditions.sh' on my homelab (Ubuntu 22.04) I get the below result meaning "==" is not supported in sh, Where as BASH supports both.
+#sh test.sh
+#ok
+#test.sh: 6: [: test: unexpected operator
+
+#Side note it seems that zsh also does not support the use of "=="
+#zsh conditions.sh
+#ok
+#conditions.sh:6: = not found
--- 4-bash/fail.sh
+++ 4-bash/fail.sh
@@ -1,4 +1,8 @@
-#!/bin/bash -e
+#!/bin/bash
+
+set -e #the flag should be explicitly set instead of being in the shebang line at the start
+#this is because when the script is run with "sh fail.sh" it uses the Bourne Shell 
+#which will ignore that first shebang line hence it will ignore the -e
 
 echo "pre"
 # due to the -e flag above this should stop the script
